b14d6e87cac42104ff9d496cf1332d731eba402a
AsenDulakshnaKavinda <s22010362@ousl.lk>
2026-09-23 20:54:07 +0530

    crated func to read pdf data from the pdf in minIO buckets

--- services/ingestion-go/go.mod
+++ services/ingestion-go/go.mod
@@ -11,7 +11,6 @@ require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.15.0 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
-	github.com/caarlos0/env/v10 v10.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
@@ -23,11 +22,11 @@ require (
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.19.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.4.0 // indirect
 	github.com/klauspost/crc32 v1.3.0 // indirect
+	github.com/ledongthuc/pdf v0.0.0-20260907135840-6c8c28e0e8a0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/minio/crc64nvme v1.1.1 // indirect
--- services/ingestion-go/pkg/pdf/parser.go
+++ services/ingestion-go/pkg/pdf/parser.go
@@ -1,2 +1,52 @@
 package pdf
 
+// This package provides functionality to parse PDF files stored in a MinIO bucket and extract their text content.
+// It uses the "github.com/ledongthuc/pdf" library to read and extract text from PDF files and the "github.com/minio/minio-go/v7" library to interact with MinIO.
+
+import (
+	"context"
+	"io"
+
+	"github.com/ledongthuc/pdf"
+	"github.com/minio/minio-go/v7"
+)
+
+// ParsePDF extracts text from a PDF file stored in a MinIO bucket.
+// It takes a context, a MinIO client, the bucket name, and the object name as parameters.
+// It returns the extracted text as a string and an error if any occurred during the process.
+func ParsePDF(ctx context.Context, client *minio.Client, bucket string, objectName string) (string, error) {
+
+	// Get the object from MinIO
+	object, err := client.GetObject(ctx, bucket, objectName, minio.GetObjectOptions{})
+	if err != nil {
+		return "", err
+	}
+	defer object.Close()
+
+	// Get the object statistics to determine its size
+	stat, err := object.Stat()
+	if err != nil {
+		return "", nil
+	}
+
+	// Create a new PDF reader from the object
+	reader, err := pdf.NewReader(object, stat.Size)
+	if err != nil {
+		return "", err
+	}
+
+	// Extract plain text from the PDF reader
+	textReader, err := reader.GetPlainText()
+	if err != nil {
+		return "", err
+	}
+
+	// Read all the text from the text reader
+	buf, err := io.ReadAll(textReader)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buf), nil
+
+}
